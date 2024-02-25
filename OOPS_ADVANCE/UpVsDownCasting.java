133bc923f7d468ed5bca37b356d3c5fc409878c0
Saurabh Maurya <sm0107870@gmail.com>
2024-02-25 21:16:09 +0530

    added MoreCode

--- OOPS_ADVANCE/UpVsDownCasting.java
+++ OOPS_ADVANCE/UpVsDownCasting.java
@@ -1,23 +1,63 @@
-package OOPS_ADVANCE;
+// package OOPS_ADVANCE;
 
-class Student{
-    String name ;
+// class Student{
+//     String name ;
 
-}
+// }
 
-class Rauank extends Student{
+// class Rauank extends Student{
 
-}
+// }
 
-public class UpVsDownCasting {
+// public class UpVsDownCasting {
 
-    public static void main(String[] args) {
-        Student student1 = new Student();
-        Student s = new Rauank();
+//     public static void main(String[] args) {
+//         Student student1 = new Student();
+//         Student s = new Rauank();
         
-        Rauank r = (Rauank) new Student(); // downcasting 
+//         Rauank r = (Rauank) new Student(); // downcasting 
 
 
-        System.out.println(student1 instanceof Student);
+//         System.out.println(student1 instanceof Student);
+//     }
+// }
+
+
+
+interface Printable {
+}
+
+class A implements Printable {
+    public void a() {
+        System.out.println("a method");
+    }
+}
+
+class B implements Printable {
+    public void b() {
+        System.out.println("b method");
+    }
+}
+
+class Call {
+    void invoke(Printable p) {// upcasting
+        if (p instanceof A) {
+            A a = (A) p;// Downcasting
+            a.a();
+            System.out.println("Ok Downcasting done for A");
+        }
+        if (p instanceof B) {
+            B b = (B) p;// Downcasting
+            b.b();
+            System.out.println("Ok Downcasting done for B");
+        }
+
+    }
+}// end of Call class
+class Test4 {
+    public static void main(String args[]) {
+        Printable p = new B();
+        Call c = new Call();
+        c.invoke(p);
     }
 }
