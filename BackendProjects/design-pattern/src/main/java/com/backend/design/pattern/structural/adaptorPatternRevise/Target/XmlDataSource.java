c6a2495399d0ee9122b043f23a108be7daec93cb
sm0107870@gmail.com <sm0107870@gmail.com>
2025-08-30 22:39:09 +0530

    added fix for compilation

---
+++ BackendProjects/design-pattern/src/main/java/com/backend/design/pattern/structural/adaptorPatternRevise/Target/XmlDataSource.java
@@ -0,0 +1,5 @@
+package com.backend.design.pattern.structural.adaptorPatternRevise.Target;
+
+public interface XmlDataSource {
+    public String getXml();
+}
---
+++ BackendProjects/design-pattern/src/main/java/com/backend/design/pattern/structural/adaptorPatternRevise/Target/convertTo/LegacyJsonService.java
@@ -0,0 +1,12 @@
+package com.backend.design.pattern.structural.adaptorPatternRevise.Target.convertTo;
+
+public class LegacyJsonService {
+
+    public LegacyJsonService() {
+
+    }
+
+    public String getJson() {
+        return "KEY:VALUE";
+    }
+}
