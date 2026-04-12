dad120fdcf058f7a6110e0c6b3dff5e6b3015afe
SaurabhMaurya <saurabh.maurya@boomi.com>
2026-04-12 12:03:17 +0530

    added a basic chat with claude using spring

---
+++ BackendProjects/artificial-intelligence/src/main/java/com/backend/ai/springai/ArtificialIntelligence.java
@@ -0,0 +1,12 @@
+package com.backend.ai.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtificialIntelligence {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtificialIntelligence.class);
+    }
+}
---
+++ BackendProjects/artificial-intelligence/src/main/java/com/backend/ai/springai/Controller/ArtificialIntelligenceController.java
@@ -0,0 +1,22 @@
+package com.backend.ai.springai.Controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ArtificialIntelligenceController {
+
+    private ChatClient _chatClient;
+
+    ArtificialIntelligenceController(ChatClient.Builder builder) {
+        this._chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("query") String query) {
+
+        return _chatClient.prompt(query).call().content();
+    }
+}
